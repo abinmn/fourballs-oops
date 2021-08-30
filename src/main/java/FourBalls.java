467b7056b209c12dd6902766c62e884561b730fb
Abin Mn <abinmn619@gmail.com>
2021-08-30 20:35:11 +0530

    Initialize processing library

---
+++ src/main/java/FourBalls.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class FourBalls extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
+    public static void main(String[] args) {
+        PApplet.main("FourBalls");
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+    }
+
+}
