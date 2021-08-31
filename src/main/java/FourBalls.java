3d0127e2a9deab809cc44b78b40c036704a1229c
Abin Mn <abinmn619@gmail.com>
2021-08-31 09:06:03 +0530

    Add methods to draw balls

--- src/main/java/FourBalls.java
+++ src/main/java/FourBalls.java
@@ -1,8 +1,15 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class FourBalls extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
+
+    public static final int TOTAL_BALLS = 4;
+
+    private int relativeHorizontalPosition = 0;
+    private ArrayList<Ball> balls = new ArrayList<>();
 
     public static void main(String[] args) {
         PApplet.main("FourBalls");
@@ -16,6 +23,25 @@ public class FourBalls extends PApplet {
 
     @Override
     public void setup() {
+        for (int b = 1; b <= TOTAL_BALLS; b++) {
+            Ball ball = new Ball(b, getRelativeHeight(b), HEIGHT,this);
+            balls.add(ball);
+        }
     }
 
+    @Override
+    public void draw() {
+        for (Ball ball: balls) {
+            ball.draw(relativeHorizontalPosition);
+        }
+        incrementRelativeHorizontalPosition();
+    }
+
+    private double getRelativeHeight(int i) {
+        return i * 0.2;
+    }
+
+    private void incrementRelativeHorizontalPosition() {
+        relativeHorizontalPosition++;
+    }
 }
